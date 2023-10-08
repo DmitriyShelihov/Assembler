6cc12d1097ebc07285c51bba499d07ffaa5ff4f7
DmitriyShelihov <142345381+DmitriyShelihov@users.noreply.github.com>
2023-10-09 02:41:22 +0300

    Completed asm

--- asm.cpp
+++ asm.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
 
 int main()
@@ -14,10 +15,9 @@ int main()
     assembly(program, bin_program);
 
     fclose(program);
-    //fclose(bin_program);
+    fclose(bin_program);
     return 0;
 }
-
 
 void assembly(FILE* program, FILE* bin_program)
 {
@@ -37,119 +37,221 @@ void assembly(FILE* program, FILE* bin_program)
 
     int sscanf_result = sscanf(source, " %s ", command);
     source += strlen(command) + 1;
+    to_lower(command);
 
     while (sscanf_result != EOF)
     {
-        puts(command);
-        if (!strcmp(command, "push")) assembly_push(bin_program, source);
-        else if (!strcmp(command, "pop")) assembly_pop(bin_program, source);
-        else if (!strcmp(command, "div")) fprintf(bin_program, "%d\n", command_div);
-        else if (!strcmp(command, "sub")) fprintf(bin_program, "%d\n", command_sub);
-        else if (!strcmp(command, "add")) fprintf(bin_program, "%d\n", command_add);
-        else if (!strcmp(command, "mul")) fprintf(bin_program, "%d\n", command_mul);
-        else if (!strcmp(command, "out")) fprintf(bin_program, "%d\n", command_out);
-        else if (!strcmp(command, "sqrt")) fprintf(bin_program, "%d\n", command_sqrt);
-        else if (!strcmp(command, "in")) fprintf(bin_program, "%d\n", command_in);
-        else if (!strcmp(command, "HLT")) fprintf(bin_program, "%d\n", command_HLT);
+        to_lower(command);
+        if (!strcmp(command, "push"))      assembly_push(bin_program, &source);
+        else if (!strcmp(command, "pop"))  assembly_pop(bin_program,  &source);
+        else if (!strcmp(command, "div"))  assembly_div(bin_program,  &source);
+        else if (!strcmp(command, "sub"))  assembly_sub(bin_program,  &source);
+        else if (!strcmp(command, "add"))  assembly_add(bin_program,  &source);
+        else if (!strcmp(command, "mul"))  assembly_mul(bin_program,  &source);
+        else if (!strcmp(command, "out"))  assembly_out(bin_program,  &source);
+        else if (!strcmp(command, "sqrt")) assembly_sqrt(bin_program, &source);
+        else if (!strcmp(command, "in"))   assembly_in(bin_program,   &source);
+        else if (!strcmp(command, "hlt"))  assembly_hlt(bin_program,  &source);
         else
         {
-            printf(REDD "%s, Syntax error\n" END_OF_COLOUR, command);
+            printf(REDD "%s, Incorrect name of function\n" END_OF_COLOUR, command);
             return;
         }
 
         sscanf_result = sscanf(source, " %s ", command);
         source += strlen(command) + 1;
-    }
 
+    }
     free(command);
     free(source);
     free(cpu_register);
 }
 
-int check_register(char* cpu_register)
+REGISTER check_register(char* c_register)
 {
-    for (int i = 0; i < strlen(cpu_register) - 2; i++)
+    for (int i = 0; i < strlen(c_register) - 2; i++)
     {
-        if (cpu_register[i] != ' ')
+        if (c_register[i] != ' ')
         {
-            if ((cpu_register[i] == 'r' || cpu_register[i] == 'R') &&
-                (cpu_register[i+2] == 'x' || cpu_register[i+2] == 'X'))
+            if ((c_register[i] == 'r' || c_register[i] == 'R') &&
+                (c_register[i+2] == 'x' || c_register[i+2] == 'X'))
             {
-                return (cpu_register[i+1] - 'a' + 1);
+                int nregister = c_register[i+1] - 'a' + 1;
+                switch (nregister)
+                {
+                    case 1: case 2: case 3: case 4:
+                        return (REGISTER)nregister;
+                    default:
+                        fprintf(stderr, RED "There is no such register\n" END_OF_COLOUR);
+                        return register_not_exist;
+                }
             }
             else
             {
                 fprintf(stderr, RED "Incorrect name of register\n" END_OF_COLOUR);
-                return -1;
+                return register_incorrect;
             }
         }
     }
-    fprintf(stderr, RED "Name if register was not found\n" END_OF_COLOUR);
-    return -2;
+    fprintf(stderr, RED "Register not specified\n" END_OF_COLOUR);
+    return register_not_found;
 }
 
 int len_of_number(double number)
 {
     if (number > 0)
-        return (int)floor (log10 (abs (number)));
-    else if (number < 0)
         return (int)floor (log10 (abs (number))) + 1;
+    else if (number < 0)
+        return (int)floor (log10 (abs (number))) + 2;
     else
         return 1;
 }
 
-void assembly_push(FILE* bin_program, char* source)
+void assembly_push(FILE* bin_program, char** source)
 {
     assert(bin_program != NULL);
-    assert(source != NULL);
+    assert(*source != NULL);
 
     fprintf(bin_program, "%d ", command_push);
-
     double variable = 0;
-    char cpu_register[SIZE_OF_REGISTER] = {};
+    char* c_register = (char*)calloc(SIZE_OF_REGISTER, sizeof(char));
 
-    if (sscanf(source, "%lf", &variable))
+    assert(c_register != NULL);
+
+    if (sscanf(*source, "%lf", &variable))
     {
-        source += len_of_number(variable) + 2;
+        *source += len_of_number(variable) + 1;
         fprintf(bin_program, "%d %lf\n", 1, variable);
     }
     else
     {
-        sscanf(source, " %s ", cpu_register);
-        source += strlen(cpu_register) + 1;
-        fprintf(bin_program, "%d %d\n", 2, check_register(cpu_register));
+        sscanf(*source, " %s ", c_register);
+        *source += strlen(c_register) + 1;
+        fprintf(bin_program, "%d %d\n", 2, check_register(c_register));
     }
+    (*source)--;
     skip_comment(source);
+    free(c_register);
 }
 
-void assembly_pop(FILE* bin_program, char* source)
+void assembly_pop(FILE* bin_program, char** source)
 {
     assert(bin_program != NULL);
-    assert(source != NULL);
+    assert(*source != NULL);
 
     fprintf(bin_program, "%d ", command_pop);
-    char cpu_register[SIZE_OF_REGISTER] = {};
+    char* c_register = (char*)calloc(SIZE_OF_REGISTER, sizeof(char));
+    sscanf(*source, " %s ", c_register);
+    *source += strlen(c_register) + 1;
 
-    sscanf(source, " %s ", cpu_register);
-    source += strlen(cpu_register) + 1;
-
-    fprintf(bin_program, "%d %d\n", 2, check_register(cpu_register));
+    fprintf(bin_program, "%d %d\n", 2, check_register(c_register));
+    (*source)--;
     skip_comment(source);
-    printf("(%c%c%c)", *source, *(source+1), *(source+2));
 }
 
-void skip_comment(char* source)
+void assembly_mul(FILE* bin_program, char** source)
 {
-    assert(source != NULL);
+    assert(bin_program != NULL);
+    assert(*source != NULL);
 
-    char symbol = *source;
-    while (symbol != ' ' && symbol != '\n' && symbol != '\0')
+    fprintf(bin_program, "%d\n", command_mul);
+    (*source)--;
+    skip_comment(source);
+}
+
+void assembly_div(FILE* bin_program, char** source)
+{
+    assert(bin_program != NULL);
+    assert(*source != NULL);
+
+    fprintf(bin_program, "%d\n", command_div);
+    (*source)--;
+    skip_comment(source);
+}
+
+void assembly_sub(FILE* bin_program, char** source)
+{
+    assert(bin_program != NULL);
+    assert(*source != NULL);
+
+    fprintf(bin_program, "%d\n", command_sub);
+    (*source)--;
+    skip_comment(source);
+}
+
+void assembly_add(FILE* bin_program, char** source)
+{
+    assert(bin_program != NULL);
+    assert(*source != NULL);
+
+    fprintf(bin_program, "%d\n", command_add);
+    (*source)--;
+    skip_comment(source);
+}
+
+void assembly_out(FILE* bin_program, char** source)
+{
+    assert(bin_program != NULL);
+    assert(*source != NULL);
+
+    fprintf(bin_program, "%d\n", command_out);
+    (*source)--;
+    skip_comment(source);
+}
+
+void assembly_sqrt(FILE* bin_program, char** source)
+{
+    assert(bin_program != NULL);
+    assert(*source != NULL);
+
+    fprintf(bin_program, "%d\n", command_sqrt);
+    (*source)--;
+    skip_comment(source);
+}
+
+void assembly_in(FILE* bin_program, char** source)
+{
+    assert(bin_program != NULL);
+    assert(*source != NULL);
+
+    fprintf(bin_program, "%d\n", command_in);
+    (*source)--;
+    skip_comment(source);
+}
+
+void assembly_hlt(FILE* bin_program, char** source)
+{
+    assert(bin_program != NULL);
+    assert(*source != NULL);
+
+    fprintf(bin_program, "%d\n", command_hlt);
+    (*source)--;
+    skip_comment(source);
+}
+
+void skip_comment(char** source)
+{
+    assert(*source != NULL);
+
+    char symbol = *(*source);
+    while (symbol != '\n' && symbol != '\0')
+    {
         if (symbol == ';')
         {
-            while (symbol != ' ' && symbol != '\n' && symbol != '\0')
-                symbol = *(source++);
+            while (symbol != '\n' && symbol != '\0')
+                symbol = *((*source)++);
         }
         else
-            symbol = *(source++);
-    return;
+            symbol = *((*source)++);
+    }
+}
+
+void to_lower(char* str)
+{
+    assert(str != NULL);
+    while (*str != '\0')
+    {
+        *str = tolower(*str);
+        str++;
+    }
 }
--- asm.h
+++ asm.h
@@ -14,7 +14,7 @@ const int SIZE_OF_REGISTER = 10;
 #define POSITION_IN_CODE __FILE__, __PRETTY_FUNCTION__, __LINE__
 
 /* this enumeration contains the assembly language command numbers */
-enum commands
+enum COMMANDS
 {
     command_push = 33,
     command_div  = 2,
@@ -22,7 +22,7 @@ enum commands
     command_out  = 4,
     command_add  = 5,
     command_mul  = 6,
-    command_HLT  = 7,
+    command_hlt  = 7,
     command_sqrt = 8,
     command_sin  = 9,
     command_cos  = 10,
@@ -30,19 +30,49 @@ enum commands
     command_pop  = 43
 };
 
+enum REGISTER
+{
+    register_rax = 1,
+    register_rbx = 2,
+    register_rcx = 3,
+    register_rdx = 4,
+    register_not_exist = -3,
+    register_incorrect = -1,
+    register_not_found = -2
+};
+
 /* a function that gives each command its own number */
 void assembly(FILE* program, FILE* bin_program);
 
 /* a function that returns the number of register or -1 || -2 if it is not register */
-int check_register(char* cpu_register);
+REGISTER check_register(char* c_register);
 
 /* a function that returns the number of characters in the number */
 int len_of_number(double number);
 
-void assembly_push(FILE* bin_program, char* source);
+void assembly_push(FILE* bin_program, char** source);
 
-void assembly_pop(FILE* bin_program, char* source);
+void assembly_pop(FILE* bin_program, char** source);
 
-void skip_comment(char* source);
+void assembly_div(FILE* bin_program, char** source);
+
+void assembly_out(FILE* bin_program, char** source);
+
+void assembly_sqrt(FILE* bin_program, char** source);
+
+void assembly_sub(FILE* bin_program, char** source);
+
+void assembly_add(FILE* bin_program, char** source);
+
+void assembly_mul(FILE* bin_program, char** source);
+
+void assembly_in(FILE* bin_program, char** source);
+
+void assembly_hlt(FILE* bin_program, char** source);
+
+void skip_comment(char** source);
+
+void to_lower(char* str);
+
 
 #endif
